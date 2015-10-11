0b5f59b7b55f3f3c85ec8e8301ee53f3ef481834
unknown <eric.vachon.pro@orange.fr>
2015-10-11 16:36:43 +0200

    ok

--- server.R
+++ server.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(RCurl)
 library(ggplot2)
+library(caret)
 
 theURL <-"https://archive.ics.uci.edu/ml/datasets/Student+Performance"
 
@@ -11,13 +12,9 @@ loadCSV <- function(URL) {
 }
 
 SelectStudentData <- function(theData){
-  
-  if (theData == "Portugues") 
-  {
+  if (theData == "Portugues") {
     studentData     <- loadCSV("https://raw.githubusercontent.com/ervachon/Developing_Data_Products/gh-pages/student/student-por.csv")   
-  }
-  else
-  {
+  } else {
     studentData     <- loadCSV("https://raw.githubusercontent.com/ervachon/Developing_Data_Products/gh-pages/student/student-mat.csv")   
   }
   
@@ -26,21 +23,43 @@ SelectStudentData <- function(theData){
   
 }
 
+theColors <- function(indice){
+  col <- array(data <- c("red", "orange", "blue", "green", "brown", "yellow", "pink"))
+  return(col[as.integer(indice)])
+} 
+
+myPredict <- function(myData){
+  inTrain           <- createDataPartition(y=myData$G3, p=.8, list = FALSE)
+  ourTrain          <- myData[inTrain,]
+  ourTest           <- myData[-inTrain,]
+  modelFit          <- train(ourTrain$G3 ~ ., method = "glm",data=ourTrain)
+  testPred          <- predict(modelFit,ourTest)
+  results           <- cbind(ourTest$G3,testPred)
+  colnames(results) <- c("Reference", "Prediction")
+  return(as.data.frame(results))
+}
 
 shinyServer(
-  function(input, output) {
-    output$selected <- renderText({ paste("You have selected : ", input$data)})
-    output$showed   <- renderText({ paste("Data : ", input$data)})
+  function(input, output) {    
+    set.seed(12345)
+        
+    myData <- reactive({SelectStudentData(input$data)})
     
-    myData <- reactive(SelectStudentData(input$data))
-    
-    output$theHTML         <- renderUI({getPage()})
+    output$selected        <- renderText({ paste("You have selected : ", input$data)})
+    output$showed          <- renderText({ paste("Data : ", input$data)})
     output$theDataSelected <- renderDataTable({data <- myData()})
     output$ageHist         <- renderPlot(ggplot(myData(), aes(x=ageLevel,fill=sex))
                                             + geom_bar(binwidth=.5,position="dodge")
                                             + facet_grid(school ~ . )
                                             + xlab ("Age of the students")
-         )
-
+                                            + scale_fill_manual(values=c( theColors(input$col1), theColors(input$col2)))
+                                         )
+    
+    myPred       <- reactive({myPredict(myData())})
+    output$pred <- renderPlot( ggplot(myPred(), aes(x = Reference, y = Prediction)) 
+                                   + geom_point(color='blue')
+                                   + geom_abline(intercept=0,slope=1,colour='red') 
+                                   + geom_smooth(color = 'green')
+                             )
   }
 )
--- ui.R
+++ ui.R
@@ -3,9 +3,10 @@
 # setwd("D:\\_MOOC_\\git\\Developing_Data_Products")
 # runApp()
 
+########################library(rCharts)
+
 library(shiny)
-library(rCharts)
-library(manipulate)
+
 
 shinyUI(pageWithSidebar(
    headerPanel(HTML('Developing Data Products Project<h2>Eric VACHON<h3>October 2015')),
@@ -15,7 +16,23 @@ shinyUI(pageWithSidebar(
                 choices = list("Portugues","Math"),
                 selected = "Portugues"
       ),
-      br(),br(),br(),br(),
+      br(),
+      h4("Select colors of Histogram"),
+        fluidRow(
+          column(4,radioButtons("col1", label = h4("Female"),
+                                 choices = list("red"   = 1, "orange" = 2, "blue"   = 3,
+                                                "green" = 4, "brown"  = 5, "yellow" = 6,
+                                                "pink"  = 7), selected = 1)),
+          column(1,radioButtons("col2", label = h4("Male"),
+                                 choices = list("red"   = 1, "orange" = 2, "blue"   = 3,
+                                                "green" = 4, "brown"  = 5, "yellow" = 6,
+                                                "pink"  = 7), selected = 3))
+      ),
+      
+      textOutput("col1Sel"),
+      textOutput("col2Sel"),
+      br(),
+      
       p("The data source : "),
       a("UC Irvine Machine Learning Repository", 
         href="https://archive.ics.uci.edu/ml/datasets/Student+Performance"),
@@ -28,6 +45,7 @@ shinyUI(pageWithSidebar(
         tabPanel("Documentation"                , includeHTML("https://raw.githubusercontent.com/ervachon/Developing_Data_Products/gh-pages/html/documentation.html")),
         tabPanel("Source ands data informations", includeHTML("https://raw.githubusercontent.com/ervachon/Developing_Data_Products/gh-pages/html/source.html")),
         tabPanel("Histograms"                   , textOutput("selected"), plotOutput("ageHist")),
+        tabPanel("Prediction"                   , plotOutput("pred")),
         tabPanel("Browse data"                  , textOutput("showed"), dataTableOutput(outputId="theDataSelected"))
          
         )
